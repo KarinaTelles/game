6514803fe6ba60596ef4b80cf0179236f42eab9c
Karina Cardoso Telles <karina@divibank.co>
2021-02-16 17:39:12 -0300

    boxes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed;
-    [SerializeField] float direction = -1;
+    [SerializeField] Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = transform.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * direction * Time.deltaTime * speed;
+        transform.position += direction * speed * Time.deltaTime;
     }
     void OnCollisionEnter2D(Collision2D col)
     {
+         
         Debug.Log("OnCollisionEnter2D");
         if (col.gameObject.CompareTag("Wall"))
         {
-            direction = direction * -1;
+            foreach (var item in col.contacts)
+            {
+                direction = 2 * Vector3.Dot(direction, Vector3.Normalize(item.normal)) * Vector3.Normalize(item.normal) - direction;
+                direction = direction * -1;
+            }
+
         }
+
     }
 }
